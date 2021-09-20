2f13c6000c8a1f7ea57e8104a9b3423c64ba6972
OlhaSi <liush.olga@gmail.com>
2021-09-20 14:27:21 +0200

    add sort chats f-n and localStorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import styles from "./App.module.css";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import LeftSide from "./modules/left-side/left-side";
 import Chat from "./modules/chat/chat";
 import ChatData from "./data/chats.json";
 
-const App = () => {
-  const [chats, setChats] = useState(ChatData.chats);
-  const [activeChat, setActiveChat] = useState(ChatData.chats[0]);
+const chatsSort = (chats) => {
+  return chats.sort((a, b) => {
+    return (
+      b.messages[b.messages.length - 1].timestamp -
+      a.messages[a.messages.length - 1].timestamp
+    );
+  });
+};
 
-  useEffect(() => {}, []);
+const STORAGE_ID = "local-chats";
+const localChatsString = localStorage.getItem(STORAGE_ID);
+const localChats = localChatsString ? JSON.parse(localChatsString) : undefined;
+
+const App = () => {
+  const [chats, setChats] = useState(localChats || ChatData.chats);
+  const [activeChat, setActiveChat] = useState(chats[0]);
+
+  useEffect(() => {
+    const sortResults = chatsSort(chats);
+    localStorage.setItem(STORAGE_ID, JSON.stringify(sortResults));
+    setChats(sortResults);
+    setActiveChat(sortResults[0]);
+  }, []);
 
   return (
     <div className={styles.App}>
